952fcc4661d991c52bd3b8ade7f763b3328dd790
AngelRamosv <angeluam.0927@gmail.com>
2025-11-06 19:18:35 -0600

    actualizando navegador

--- factura.py
+++ factura.py
@@ -18,7 +18,7 @@ def run(url, rfc, total, proveedor):
     opts.add_argument("--allow-file-access-from-files")
 
     # Evita conflictos de perfiles de usuario (no usar user-data-dir)
-    # opts.add_argument("--headless=new")              # modo sin interfaz gráfica
+    opts.add_argument("--headless=new")              # modo sin interfaz gráfica
     opts.add_argument("--disable-gpu")               # evita errores de GPU
     opts.add_argument("--disable-software-rasterizer")
     opts.add_argument("--remote-debugging-port=9222") # usa un puerto interno único
